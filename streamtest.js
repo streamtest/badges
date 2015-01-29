0c3e5f473a810ae50461f2109b11758cc205077d
BitFlicker5000 <matthew@netro.ca>
2015-01-29 12:40:04 -0800

    swap css urls

--- streamtest.js
+++ streamtest.js
@@ -37,7 +37,7 @@ if (!window.jQuery) {
     include("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", "js")
 }
 
-include("https://streamtest.blob.core.windows.net/styles/streamtestbadge.css", "css");
+include("https://netrodev.github.io/streamtest-badge/streamtestbadge.css", "css");
 
 function jqueryLoaded() {
     clearInterval(jQueryLoadCheckInterval);
